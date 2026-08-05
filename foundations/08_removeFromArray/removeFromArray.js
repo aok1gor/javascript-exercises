e46335ff1b6a09aa50c1d9f34d6c46357423a5ec
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-05 16:00:11 -0300

    Do exercise 8 of js foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+
+    return result;
+
 };
 
 // Do not edit below this line
