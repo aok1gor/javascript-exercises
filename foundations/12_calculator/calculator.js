f2d19c4ad7c30ac3f06e34d2511e92e23f3e838a
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-05 17:04:18 -0300

    Do exercise 12 of js foundations

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1-number2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let result = 0;
+
+  numbers.forEach((number) => {
+    result += number;
+  });
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
 
+  let result = 1;
+  numbers.forEach((number) => {
+    result *= number;
+  });
+  return result;
 };
 
-const power = function() {
+const power = function(base, expoent) {
+
+  let result = base;
+
+  for (let i = 1; i < expoent; i++) {
+    result *= base;
+  }
 	
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(number) {
 	
+  if (number == 0) {
+    return 1;
+  }
+
+  let result = 1;
+
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+
+
+  return result;
 };
 
 // Do not edit below this line
