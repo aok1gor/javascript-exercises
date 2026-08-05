836f795670ac144ba7dbbff8fc82a64ad6e79e07
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-05 16:39:42 -0300

    Do exercise 11 of js foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+  let result = ((f - 32) * 5 / 9).toFixed(1);
+
+  if (result == 0) {
+    return 0;
+    }
+
+  return Number(result);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let result = ((c * 9 / 5) + 32).toFixed(1);
+
+  if (result == 0) {
+    return 0;
+  }
+
+  return Number(result);
 };
 
 // Do not edit below this line
