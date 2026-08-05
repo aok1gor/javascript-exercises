2f29e5a92c25ac705ddda6b32d3b0e8a20cb4d9b
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-05 16:16:29 -0300

    Do exercise 9 of js foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    if (Number.isInteger(number1) && Number.isInteger(number2) && number1 > 0 && number2 > 0) {
+
+        let sum = 0;
+
+        let difference = Math.abs(number1 - number2);
+
+        let bigger = 0;
+        let smaller = 0;
+
+        if (number1 > number2) {
+            bigger = number1;
+            smaller = number2;
+        }
+
+        else {
+            bigger = number2;
+            smaller = number1;
+        }
+
+        sum = (bigger + smaller) * (bigger - smaller + 1) / 2;
+
+        return sum;
+    }
+
+    else{
+        return `ERROR`;
+    }
 };
 
 // Do not edit below this line
