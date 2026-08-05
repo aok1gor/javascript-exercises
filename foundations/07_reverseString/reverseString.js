68fe70f389ff07eac0b24b7ad560deeb3aeaafc9
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-05 15:37:22 -0300

    Do exercise 7 of js foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let length = word.length;
 
+    let result = "";
+
+    for (let j = length - 1; j >= 0; j--) {
+        result += word[j];
+        
+    }
+
+    return result
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
