edb74475df3c312a7d51e003834a3750a427cbb5
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-06 10:16:00 -0300

    Do exercise 16 of js foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let ageOfDeath = 0;
+    let biggestAge = 0;
+    let numberOfBiggest = 0;
+
+    for (let i = 0; i < people.length; i++) {
+        
+        if (!("yearOfDeath" in people[i])) {
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+
+        ageOfDeath = people[i].yearOfDeath - people[i].yearOfBirth;
+        
+        
+        if (ageOfDeath > biggestAge) {
+            biggestAge = ageOfDeath;
+            numberOfBiggest = i;
+        }
+    }
+
+    return people[numberOfBiggest];
 
 };
 
