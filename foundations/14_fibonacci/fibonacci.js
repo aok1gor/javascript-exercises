a2c1a80d36f7dd954dd15e93b6fa01be3520864c
Igor Hideki Aoki <307203727+aok1gor@users.noreply.github.com>
2026-08-06 09:44:18 -0300

    Do exercise 14 of js foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    let position = Number(input);
+
+    let number1 = 1;
+    let number2 = 1;
+    let aux = 0;
+
+    for (let i = 0; i < position - 2; i++) {
+        aux = number2;
+        number2 = number1 + number2;
+        number1 = aux;
+    }
+
+    if (position == 1 || position == 2) {
+        return 1;
+    }
+
+    else if (position == 0 || position == "0") {
+        return 0;
+    }
+
+    else if (position < 0) {
+        return "OOPS";
+    }
+
+    else {
+        return number2;
+    }
 };
 
 // Do not edit below this line
